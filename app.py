f63c95bc289154f752d686c8bdb499cea899056d
kkby52 <kkby2022@chungbuk.ac.kr>
2025-06-07 23:27:34 +0900

    신뢰도 계산 기능 추가 및 Flask 통합

--- app.py
+++ app.py
@@ -10,5 +10,5 @@ def index():
     return render_template('index.html', news_list=news_list)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
