aa30dcb295a73478653bcaab48fe88c2f27b13a4
kkby52 <kkby2022@chungbuk.ac.kr>
2025-06-07 20:35:27 +0900

    Fix: updated app.py to support Render deployment

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 from news_crawler import get_news_list
 
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,4 +11,6 @@ def index():
     return render_template('index.html', news_list=news)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT",5000))
+    
+    app.run(host="0.0.0.0", port=port)
