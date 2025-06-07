a410a8ad70ab58e1cc0a11a058e83d50e5e243c9
kkby52 <kkby2022@chungbuk.ac.kr>
2025-06-07 17:49:16 +0900

    first commit

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
-from news_crawler import get_naver_news_titles  # 위 코드를 news_crawler.py로 저장했다고 가정
+from news_crawler import get_news_list
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    news = get_naver_news_titles()
+    news = get_news_list()
     return render_template('index.html', news_list=news)
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(debug=True)
