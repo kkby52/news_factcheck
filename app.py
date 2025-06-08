71f9b16333e77c84a1d162970bc6e8605994fac2
kkby52 <kkby2022@chungbuk.ac.kr>
2025-06-08 20:55:46 +0900

    신뢰도 평가 기능 추가 및 Flask 통합

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ def index():
     news_list = get_news_list_with_similarity()
     return render_template('index.html', news_list=news_list)
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    host = "0.0.0.0"
+    port = int(os.getenv("PORT", "10000"))
+    app.run(host=host, port=port)
